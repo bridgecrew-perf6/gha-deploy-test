43ca146bf2ef94ce0e33e7dc65e501d746729ec1
Sven Marnach <sven@mozilla.com>
2022-01-24 21:32:45 +0100

    Allow configuring the image tag.

--- projects/nginx/tf/main.tf
+++ projects/nginx/tf/main.tf
@@ -10,7 +10,16 @@ terraform {
   }
 }
 
+variable "image_tag" {
+  type = string
+  default = "latest"
+}
+
 resource "helm_release" "example" {
   name       = "nginx"
   chart      = "../charts/nginx"
+  set {
+    name = "image.tag"
+    value = var.image_tag
+  }
 }
